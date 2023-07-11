161cf82e8e8f6872cd4a1050f494e4117a7d7726
abdullahabuharb <134230587+abdullahabuharb@users.noreply.github.com>
2023-07-10 21:20:21 -0400

    Add files via upload

---
+++ VirtualPet.java
@@ -0,0 +1,88 @@
+public class VirtualPet {
+    private int hunger;
+    private int thirst;
+    private int boredom;
+    private int tiredness;
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public void setThirst(int thirst) {
+        this.thirst = thirst;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+    public void setBoredom(int boredom) {
+        this.boredom = boredom;
+    }
+
+    public int getTiredness() {
+        return tiredness;
+    }
+
+    public void setTiredness(int tiredness) {
+        this.tiredness = tiredness;
+    }
+
+    public void eatM() {
+        System.out.println("Pet is eating medium meal.");
+        if(getHunger()>=3){setHunger(hunger - 2);};
+        if(getThirst()<=18){setThirst(thirst + 2);};
+    }
+
+    public void eatL() {
+        System.out.println("Pet is eating large meal.");
+        if(getHunger()>=4){setHunger(hunger - 3);};
+        if(getThirst()<=17){setThirst(thirst + 3);};
+    }
+
+    public void drinkM() {
+        System.out.println("Pet is drinking medium amount of water.");
+        if(getThirst()>=3){setThirst(thirst - 2);};
+        if(getBoredom()<=18){setBoredom(boredom + 2);};
+    }
+
+    public void drinkL() {
+        System.out.println("Pet is drinking large amount of water.");
+        if(getThirst()>=4){setThirst(thirst - 3);};
+        if(getBoredom()<=17){setBoredom(boredom + 3);};
+    }
+
+
+    public void playM() {
+        System.out.println("Pet is playing for two mins.");
+        if(getBoredom()>=3){ setBoredom(boredom - 2);};
+        if(getTiredness()<=18){setTiredness(tiredness + 2);};
+    }
+
+    public void playL() {
+        System.out.println("Pet is playing for three mins.");
+        if(getBoredom()>=4){ setBoredom(boredom - 3);};
+        if(getTiredness()<=17){setTiredness(tiredness + 3);};
+    }
+
+    public void sleepM() {
+        System.out.println("Pet is sleeping for five  mins.");
+        if(getTiredness()>=3){setTiredness(tiredness - 2);};
+        if(getHunger()<=18){setHunger(hunger + 2);};
+    }
+
+    public void sleepL() {
+        System.out.println("Pet is sleeping for ten mins.");
+        if(getTiredness()>=4){setTiredness(tiredness - 3);};
+        if(getHunger()<=17){setHunger(hunger + 3);};
+    }
+
+}
---
+++ VirtualPetApplication.java
@@ -0,0 +1,84 @@
+// Virtual pet done by Abdullah Abuharb and Nour Abutaha
+import java.util.Random;
+import java.util.Scanner;
+public class VirtualPetApplication extends VirtualPet {
+
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        int userInput,userInput1;
+        
+        //creating Random number to use it for th obj att value
+        Random random = new Random();
+        int min=5,max=20;
+        int randomNum1=random.nextInt(max - min +1)+min;
+        int randomNum2=random.nextInt(max - min +1)+min;
+        int randomNum3=random.nextInt(max - min +1)+min;
+        int randomNum4=random.nextInt(max - min +1)+min;
+        
+        VirtualPet pet = new VirtualPet();
+        pet.setHunger(randomNum1);
+        pet.setThirst(randomNum2);
+        pet.setBoredom(randomNum3);
+        pet.setTiredness(randomNum4);
+        
+
+        boolean i =true;
+        while(i== true){
+
+            System.out.println("Hunger: "+ pet.getHunger()+"\n"+"thirst: "+pet.getThirst()+"\n"+
+                                "Boredom: "+pet.getBoredom()+"\n"+ "Tiredness: "+pet.getTiredness());
+
+            System.out.println("What you want to do ? \n"+ "1. Feed Pet \n"+ "2. Water Pet \n"+
+                                "3. Play with Pet \n"+"4. Put Pet to sleep \n"
+                                +"Enter any other number to leave the Pet alone");
+            
+            userInput = scanner.nextInt();
+            if(userInput == 1){
+                System.out.println("1. Medium meal "+"\n"+"2. large meal ");
+                userInput1= scanner.nextInt();
+                if(userInput1 == 1){
+                  pet.eatM();}
+                else{
+                  pet.eatL();
+                }
+               }
+
+            else if (userInput == 2){
+                System.out.println("1. Medium amount of water"+"\n"+"2. Large amount of water ");
+                userInput1= scanner.nextInt();
+                if(userInput1 == 1){
+                  pet.drinkM();}
+                else{
+                  pet.drinkL();
+                }
+             }
+
+             else if(userInput == 3){
+               System.out.println("1. Play for three mins"+"\n"+"2. Play for five mins");
+               userInput1=scanner.nextInt();
+               if(userInput1 == 1){ 
+                  pet.playM();}
+               else{
+                  pet.playL();
+               }
+             }
+             else if (userInput == 4){
+               System.out.println("1. Sleep for five mins"+"\n"+ "2. Sleep for ten mins");
+               userInput1=scanner.nextInt();
+               if(userInput1 == 1){
+                  pet.sleepM();}
+               else{
+                  pet.sleepL();
+               }
+             }
+             else {
+                System.out.println("goodbye");
+                i=false;
+             }
+             
+        };
+
+    }
+    
+}
